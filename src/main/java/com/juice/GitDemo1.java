c82bc52156bf855af4063047a8cab02ccb9f09bc
chuhsi <69375430+chuhsi@users.noreply.github.com>
2021-05-16 10:39:40 +0800

    hot-fix commit

--- src/main/java/com/juice/GitDemo1.java
+++ src/main/java/com/juice/GitDemo1.java
@@ -5,5 +5,6 @@ public class GitDemo1 {
         System.out.println("hello git");
         System.out.println("hello git");
         System.out.println("hello git");
+        System.out.println("hello hot-fix");
     }
 }
