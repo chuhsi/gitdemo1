4e849c1d24b1c2c54ab3703cee85ab9e467094c2
chuhsi <69375430+chuhsi@users.noreply.github.com>
2021-05-16 11:13:20 +0800

    Update GitDemo1.java

--- src/main/java/com/juice/GitDemo1.java
+++ src/main/java/com/juice/GitDemo1.java
@@ -3,9 +3,5 @@ package com.juice;
 public class GitDemo1 {
     public static void main(String[] args) {
         System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello test2");
-        System.out.println("hello test");
     }
 }
